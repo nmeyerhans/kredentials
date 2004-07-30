69215a95c941776317abd4697873885b3984e36d
noahm <noahm@27be60a4-1c20-0410-b25d-f18e52ada5bf>
2004-07-30 16:38:09 +0000

    early development
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail/u/n/noahm/src/REPOS/kredentials/trunk@8 27be60a4-1c20-0410-b25d-f18e52ada5bf

--- src/kredentials.cpp
+++ src/kredentials.cpp
@@ -23,18 +23,91 @@
 
 #include <kmainwindow.h>
 #include <klocale.h>
+#include <kdebug.h>
+
+#include "krb_defs.h"
+#include <krb5.h>
 
 kredentials::kredentials()
-    : KMainWindow( 0, "kredentials" )
+    : KSystemTray()
 {
     // set the shell's ui resource file
-    setXMLFile("kredentialsui.rc");
+    //setXMLFile("kredentialsui.rc");
 
-    new QLabel( "Hello World", this, "hello label" );
+	menu = new QPopupMenu();
+	renewAct = new KAction(i18n("&Renew credentials"), "1rightarrow", 0,
+                               this, SLOT(renewTickets()), actionCollection(), "renew")
+
+	kerror = 0;
+	k5_opts = (k_opts*)malloc(sizeof(struct k_opts));
+		k5_opts->action = INIT_PW;
+
+	k5 = (k5_data*)malloc(sizeof(struct k5_data));
+	memset(k5, 0, sizeof(*k5));
+	err_code = krb5_init_context(&k5->ctx);
+	if(err_code)
+		kdDebug() << "Kerberos returned " << err_code << endl;
+	err_code = krb5_cc_default(k5->ctx, &k5->cc);
+	if(err_code)
+		kdDebug() << "Kerberos returned " << err_code << endl;
+	kdDebug() << "Set k5->cc to " << k5->cc << endl;
+
 }
 
 kredentials::~kredentials()
 {
 }
 
+void kredentials::mousePressEvent(QMouseEvent *e)
+{
+	if(event->button() == RightButton)
+	{
+		menu->popup(QCursor::pos());
+	}
+}
+
+int kredentials::renewTickets()
+{
+		int notix = 1;
+		krb5_keytab keytab = 0;
+		krb5_creds my_creds;
+		krb5_error_code code = 0;
+		krb5_get_init_creds_opt options;
+		//const char *err_mesg;
+
+		krb5_get_init_creds_opt_init(&options);
+		memset(&my_creds, 0, sizeof(my_creds));
+
+		err_code = krb5_cc_get_principal(k5->ctx, k5->cc,
+				&k5->me);
+		if(err_code)
+		{
+				kdDebug() << "Kerberos returned " << err_code << endl;
+				return err_code;
+		}
+		err_code = krb5_get_renewed_creds(k5->ctx, &my_creds, k5->me, k5->cc,
+								k5_opts->service_name);
+
+		if(code)
+		{
+				//err_mesg = error_message(code);
+				//kdDebug() << "Kerberos returned " << code << ": " << err_mesg << endl;
+				kdDebug() << "Kerberos returned " << code << endl;
+				return code;
+		}
+	err_code = krb5_cc_initialize(k5->ctx, k5->cc, k5->me);
+		if(err_code)
+		{
+				kdDebug() << "Kerberos returned " << err_code << endl;
+				return code;
+		}
+		err_code = krb5_cc_store_cred(k5->ctx, k5->cc, &my_creds);
+		if(err_code)
+		{
+				kdDebug() << "Kerberos returned " << err_code << endl;
+				return code;
+		}
+		return 0;
+
+}
 #include "kredentials.moc"
