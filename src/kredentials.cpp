df016b6e57b879b6410f0e210b052b145b3672ca
noahm <noahm@27be60a4-1c20-0410-b25d-f18e52ada5bf>
2004-09-08 15:46:56 +0000

    fix icon name
    git-svn-id: svn+ssh://svn.csail.mit.edu/afs/csail/u/n/noahm/src/REPOS/kredentials/trunk@55 27be60a4-1c20-0410-b25d-f18e52ada5bf

--- src/kredentials.cpp
+++ src/kredentials.cpp
@@ -55,7 +55,7 @@ kredentials::kredentials( int notify, int aklog )
 	doNotify = notify;
 	doAklog  = aklog;
 	secondsToNextRenewal = DEFAULT_RENEWAL_INTERVAL;
-	this->setPixmap(this->loadIcon("panel"));
+	this->setPixmap(this->loadIcon("kredentials"));
 	menu = new QPopupMenu();
 	//menu->insertItem("Renew Tickets", this, SLOT(renewTickets()), CTRL+Key_R);
 	//menu->insertItem("Exit", i18n("Quit"), KApplication::kApplication(), SLOT(quit()));
